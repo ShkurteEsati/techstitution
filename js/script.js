6941b0f209941accd221fc03e5f3737e25952106
unknown <ShkurteEsati>
2017-11-23 10:33:32 +0100

    Structure files are added

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+console.log(5+6);
+console.log("Hello World");
+window.alert("You visited Techstituion");
+
+
+
+function consoleName(name)
+{
+console.log("Emri im eshte:",name);	
+}
+
+consoleName("Shkurte");
+
+function Paraqitja (Emri, Mbiemri,Vendlindja, Universiteti)
+{
+	console.log ("");
+}
+Paraqitja(Shkurte Esati, Ferizaj, FIEK);
