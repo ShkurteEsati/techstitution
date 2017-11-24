cf1299b7fe57c47d948bf89850582a980aaaa808
unknown <ShkurteEsati>
2017-11-24 14:34:46 +0100

    Adding Json structure

--- js/script.js
+++ js/script.js
@@ -20,10 +20,15 @@ function Paraqitja (Emri,Vendlindja, Universiteti)
 Paraqitja("Shkurte Esati","Ferizaj", "FIEK"); */
 
 $(document).ready(function(){
-console.log("DOM is ready!")
+
+	var datetimeNow = new Date();
+
+    console.log("DOM is ready!", datetimeNow);
 
   $('#border-wait-form').submit(function(event){
        event.preventDefault();
+
+       var datetimeNow = $ ('#datetime-now').val();
 
        var pikaKufitare = $('#pika-kufitare').val();
        // vonesat ne hyrje dhe ne dalje
@@ -38,7 +43,28 @@ console.log("DOM is ready!")
        var daljeMinMetra = $('#dalje-min-metra').val();
        var daljeMaxMetra = $('#dalje-max-metra').val();
 
-       console.log("Pika Kufitare:",pikaKufitare,'\n',"Hyrje Min:",hyrjeMinMinuta,'\n',"Hyrje Max:", hyrjeMaxMinuta,'\n',"Dalje Min:",daljeMinMinuta,'\n',"Dalje Max:",daljeMaxMinuta,'\n',"Hyrje Min Metra:",hyrjeMinMetra,'\n',"Hyrje MAx Metra:",hyrjeMaxMetra,'\n',"Dalje Min Metra:",daljeMinMinuta,'\n',"Dalje Max Metra:",daljeMaxMetra,'\n')
+       //var dataJson = {"pikaKufitare": pikaKufitare};//
+       // e njejta me kta poshte//
+       var dataJson={};
+        dataJson["pikaKufitare"] = pikaKufitare;
+
+        dataJson["hyrjeMinMinuta"] = parseInt(hyrjeMinMinuta);
+        dataJson["hyrjeMaxMinuta"] = parseInt(hyrjeMaxMinuta);
+
+        dataJson["daljeMinMinuta"] = parseInt(daljeMinMinuta);
+        dataJson["daljeMaxMinuta"] = parseInt(daljeMaxMinuta);
+
+        dataJson["hyrjeMinMetra"] = parseInt(hyrjeMinMetra);
+        dataJson["hyrjeMaxMetra"] = parseInt(hyrjeMaxMetra);
+
+        dataJson["daljeMinMetra"] = parseInt(daljeMinMetra);
+        dataJson["daljeMaxMetra"] = parseInt(daljeMaxMetra);
+
+        dataJson ["datetimeNow"] = new Date();
+         
+        console.log(dataJson);
+
+        // console.log("Pika Kufitare:",pikaKufitare,'\n',"Hyrje Min:",hyrjeMinMinuta,'\n',"Hyrje Max:", hyrjeMaxMinuta,'\n',"Dalje Min:",daljeMinMinuta,'\n',"Dalje Max:",daljeMaxMinuta,'\n',"Hyrje Min Metra:",hyrjeMinMetra,'\n',"Hyrje MAx Metra:",hyrjeMaxMetra,'\n',"Dalje Min Metra:",daljeMinMinuta,'\n',"Dalje Max Metra:",daljeMaxMetra,'\n')
 
   });
   
